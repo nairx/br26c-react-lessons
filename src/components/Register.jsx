072f50e168c082b56a61a2d5cf45388be75e73e9
nairx <nairxprv@gmail.com>
2026-04-24 14:57:09 +0530

    updated

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../App";
+import { useContext } from "react";
 export default function Register() {
+  const { users, setUsers } = useContext(AppContext);
+  const [ user, setUser ] = useState({});
+  const handleSubmit = () => {
+    setUsers([...users, user]);
+  };
   return (
     <div>
       Register
       <p>
-        <input type="text" placeholder="Name" />
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
+          placeholder="Name"
+        />
       </p>
       <p>
-        <input type="text" placeholder="Email" />
+        <input
+          type="text"
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+          placeholder="Email"
+        />
       </p>
       <p>
-        <input type="password" placeholder="Password" />
+        <input
+          type="password"
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+          placeholder="Password"
+        />
       </p>
-      <button>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
       <p>
         <Link to="/login">Already a member? Login here...</Link>
       </p>
