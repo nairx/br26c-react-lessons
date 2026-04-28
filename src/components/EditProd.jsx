77fe8a5ce724a743833f723b1bac4e42e75929cc
nairx <nairxprv@gmail.com>
2026-04-28 16:10:55 +0530

    updated

--- src/components/EditProd.jsx
+++ src/components/EditProd.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useFetcher, useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 export default function EditProd() {
   const { id } = useParams();
